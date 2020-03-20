0a5f65fabbfec650dca05170eb6a5642f1ea241e
Alok Swain <alokswaincontact@gmail.com>
2020-03-20 16:17:24 -0500

    Added code for Go Slice index and Capacity

---
+++ capacity.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  x := [...]int {1, 2, 3, 4, 5}
+
+  y := x[0:2] //Last index is not included
+  z := x[1:4] //Capacity from index 1 to end
+
+  fmt.Println(len(y), cap(y), len(z), cap(z)) //2 5 3 4
+}
